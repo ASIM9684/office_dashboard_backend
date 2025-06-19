3780213f0f63ac0bd685b04e53215c348dfe4b0c
AASIM <asim.mughal.granule@gmail.com>
2025-06-19 13:02:43 +0500

    fix: validate user ID in getPendingTasksByUser function

--- controller/fetchContoller.js
+++ controller/fetchContoller.js
@@ -311,16 +311,21 @@ const getPendingTasksByUser = async (req, res) => {
   try {
     const { userId } = req.params;
 
-    const tasks = await Task.find({
-         assignedBy: userId ,
-         status : "Pending"
-    }).populate('assignedTo', 'name email')
-      .populate('assignedBy', 'name email');
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required." });
+    }
 
-    res.status(200).json(tasks);
+    const tasks = await Task.find({
+      assignedTo: userId,
+      status: "Pending"
+    })
+      .populate('assignedTo', 'name email')
+      .populate('assignedBy', 'name email')
+
+    return res.status(200).json(tasks);
   } catch (error) {
     console.error("Error fetching tasks by user:", error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
