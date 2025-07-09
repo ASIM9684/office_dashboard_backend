22e997853f93cf8985f8371ba26135677347768c
AASIM <asim.mughal.granule@gmail.com>
2025-07-09 12:35:57 +0500

    fix: update server to use PORT from environment variable

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const cors = require('cors');
 const connectDB = require('./database/database');
 const router = require('./route/routes');
 const useragent = require("express-useragent");
+require("dotenv").config();
+
 connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(useragent.express());
 app.use(router);
 
+const PORT = process.env.PORT || 8000
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 8000');
 });
